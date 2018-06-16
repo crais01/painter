5ef2e8ecb22b1546a3cab94ac3d300ef2e374ec6
crais01 <ccortesj.01@gmail.com>
2018-06-16 02:04:12 -0400

    estilo formulario y validacion de modelo

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -69,6 +69,6 @@ class PinsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
-      params.require(:pin).permit(:photo, :desciption)
+      params.require(:pin).permit(:photo, :desciption, :name)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_11_001326) do
+ActiveRecord::Schema.define(version: 2018_06_16_055009) do
 
   create_table "pins", force: :cascade do |t|
     t.string "photo"
     t.string "desciption"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
 end
